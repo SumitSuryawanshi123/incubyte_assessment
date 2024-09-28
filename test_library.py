b34fab20375b010c86c8a5f93ab786f6c3026e2b
Sumit Suryawanshi <suryawanshisumit2003@gmail.com>
2024-09-28 09:56:31 +0530

    Add test for viewing available books

--- test_library.py
+++ test_library.py
@@ -23,6 +23,17 @@ class TestLibrary(unittest.TestCase):
         library.borrow_book("123456789")
         library.return_book("123456789")
         self.assertTrue(book.is_available)
+    
+    def test_view_available_books(self):
+        library = Library()
+        book1 = Book("123456789", "Test Book 1", "Author 1", 2024)
+        book2 = Book("987654321", "Test Book 2", "Author 2", 2023)
+        library.add_book(book1)
+        library.add_book(book2)
+        library.borrow_book("123456789")
+        available_books = library.get_available_books()
+        self.assertIn(book2, available_books)
+        self.assertNotIn(book1, available_books)
 
 if __name__ == "__main__":
     unittest.main()
